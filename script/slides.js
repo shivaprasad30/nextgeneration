12e548f65d6bb222c76fecc015b55ac6c5f20e3c
pavan reddy <pavanreddysheelam@gmail.com>
2023-07-16 19:14:10 +0530

    updated slides

---
+++ script/slides.js
@@ -0,0 +1,18 @@
+var img = document.getElementById('img');
+
+var slides=['images/agri2.jpg','images/agri3.jpg','images/agri1.jpg'];
+
+var Start=0;
+
+function slider(){
+    if(Start<slides.length){
+        Start=Start+1;
+    }
+    else{
+        Start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img class='imgstyle' src="+slides[Start-1]+">";
+   
+}
+setInterval(slider,2000);
