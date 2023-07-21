d4f402f13c8b2a1ef1fbddd07c94e3a41a17b963
Shiva S <unknownperson14s41@gmail.com>
2023-07-21 22:59:04 +0530

    nev_bar,pest page

--- script/slides.js
+++ script/slides.js
@@ -35,3 +35,55 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+var droppedFiles = false;
+var fileName = '';
+var $dropzone = $('.dropzone');
+var $button = $('.upload-btn');
+var uploading = false;
+var $syncing = $('.syncing');
+var $done = $('.done');
+var $bar = $('.bar');
+var timeOut;
+
+$dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+})
+	.on('dragover dragenter', function () {
+		$dropzone.addClass('is-dragover');
+	})
+	.on('dragleave dragend drop', function () {
+		$dropzone.removeClass('is-dragover');
+	})
+	.on('drop', function (e) {
+		droppedFiles = e.originalEvent.dataTransfer.files;
+		fileName = droppedFiles[0]['name'];
+		$('.filename').html(fileName);
+		$('.dropzone .upload').hide();
+	});
+
+$button.bind('click', function () {
+	startUpload();
+});
+
+$("input:file").change(function () {
+	fileName = $(this)[0].files[0].name;
+	$('.filename').html(fileName);
+	$('.dropzone .upload').hide();
+});
+
+function startUpload() {
+	if (!uploading && fileName != '') {
+		uploading = true;
+		$button.html('Uploading...');
+		$dropzone.fadeOut();
+		$syncing.addClass('active');
+		$done.addClass('active');
+		$bar.addClass('active');
+		timeoutID = window.setTimeout(showDone, 3200);
+	}
+}
+
+function showDone() {
+	$button.html('Done');
+}
